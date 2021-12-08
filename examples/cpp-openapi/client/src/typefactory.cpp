462deb07fe66c7ba7d679dc3f0857d27c9bb1634
Youyuan Wu <youyuanwu@outlook.com>
2021-12-07 16:52:26 -0800

    add option to choose client providers

--- examples/cpp-openapi/client/src/typefactory.cpp
+++ examples/cpp-openapi/client/src/typefactory.cpp
@@ -1,12 +1,18 @@
 #include "typefactory.h"
 
-#include "beastclient.h"
+#ifdef OPENAPI_CLIENT_CURLPP
 #include "curlppclient.h"
+#elif defined(OPENAPI_CLIENT_BREAST)
+#include "beastclient.h"
+#endif
 
 std::shared_ptr<IClient> TypeFactory::NewClient(ClientConfig cfg)
 {
-    //std::shared_ptr<IClient> ret  = std::make_shared<BeastClient>(cfg);
+#ifdef OPENAPI_CLIENT_CURLPP
     std::shared_ptr<CurlPPClient> ret = std::make_shared<CurlPPClient>(cfg);
     ret->SetDebug(true);
+#elif defined(OPENAPI_CLIENT_BREAST)
+    std::shared_ptr<IClient> ret  = std::make_shared<BeastClient>(cfg);
+#endif
     return ret;
 }
