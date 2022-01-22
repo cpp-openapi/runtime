0a829600d600227c156650b79617a9f190c8345d
Youyuan Wu <youyuanwu@outlook.com>
2022-01-21 22:48:02 -0800

    strconv safe for wchar

--- runtime/include/strconv.h
+++ runtime/include/strconv.h
@@ -1,40 +1,91 @@
 #pragma once
 #include "runtime_types.h"
 
-#include <cstring> // strlen
+// #include <cstring> // strlen
+#include <cwchar>
+#include <cassert>
 
 namespace openapi{
 
 // can convert from char* to string or wstring
-static string_t StringT(const char* text)
+static string_t StringT(const char* mbstr)
 {
 #ifdef OPENAPI_UTF16_STRINGS
-    const size_t size = std::strlen(text);
-        std::wstring wstr;
-        if (size > 0) {
-            wstr.resize(size);
-            std::mbstowcs(&wstr[0], text, size);
-        }
-        return static_cast<string_t>(wstr);
+    std::size_t size{};
+    std::size_t size2{};
+    std::mbstate_t state = std::mbstate_t();
+#ifdef WIN32 
+    // windows we have the safe convert function
+    // will count null terminator in the size.
+    ::mbsrtowcs_s(&size, nullptr, 0 /*dst max size*/,  &mbstr, 0, &state);
 #else
-    return std::string(text);
+    size = std::mbsrtowcs(nullptr, &mbstr, 0, &state);
 #endif
+    std::wstring wstr;
+    if(size <= 0)
+    {
+        return static_cast<string_t>(wstr);
+    }
+    wstr.resize(size); // wtr has null terminator by itself.
+#ifdef WIN32
+    ::mbsrtowcs_s(&size2, &wstr[0], size  /*dst max size*/ , &mbstr, size, &state);
+#else
+    size2 = std::mbsrtowcs(&wstr[0], &mbstr, size, &state);
+#endif
+    assert(size2 == size);
+
+#ifdef WIN32
+    if(wstr[size-1] == L'\0')
+    {
+        // terminator is redundant.
+        wstr.resize(size-1);
+    }
+#endif
+    return static_cast<string_t>(wstr);
+
+#else  // OPENAPI_UTF16_STRINGS
+    return std::string(mbstr);
+#endif // OPENAPI_UTF16_STRINGS
 }
 
 // TODO: use const expr?
 static std::string ToStdString(string_t text)
 {
 #ifdef OPENAPI_UTF16_STRINGS
-    const size_t size = text.size();
-        std::string str;
-        if (size > 0) {
-            str.resize(size);
-            std::wcstombs(&str[0], text.c_str(), size);
-        }
-        return str;
+    std::size_t size{};
+    std::size_t size2{}; 
+    std::string str;
+    std::mbstate_t state = std::mbstate_t();
+    const wchar_t * wstr = text.c_str();
+
+#ifdef WIN32
+    ::wcsrtombs_s(&size, nullptr, 0/*dest cap*/, &wstr, 0, &state);
 #else
-    return text;
+    size = wcsrtombs(NULL, &wstr, 0, &state);
 #endif
+    if (size <= 0) {
+        return str;
+    }
+    str.resize(size);
+#ifdef WIN32
+    ::wcsrtombs_s(&size2, &str[0], size/*dest cap*/, &wstr, size, &state);
+#else
+    size2 = wcsrtombs(&str[0], &wstr, size, &state);
+#endif
+    assert(size == size2);
+
+#ifdef WIN32
+    // strip the redundant null terminator
+    if(str[size-1] == '\0')
+    {
+        // terminator is redundant.
+        str.resize(size-1);
+    }
+#endif
+    return str;
+#else  // OPENAPI_UTF16_STRINGS
+    return text;
+#endif // OPENAPI_UTF16_STRINGS 
 }
 
 }
---
+++ runtime/test/strconv_test.cpp
@@ -0,0 +1,29 @@
+#include "gtest/gtest.h"
+#include "strconv.h"
+
+TEST(StrConv_test, basic) {
+#ifdef OPENAPI_UTF16_STRINGS
+        // std::mbstate_t state = std::mbstate_t();
+        // std::size_t size;
+        // const char * buf = "";
+        // ::mbsrtowcs_s(&size, nullptr, 0 /*dst max size*/, &buf, 0, &state);
+        // ASSERT_EQ(1, size); // counts the null terminator
+
+        // const char * mbstr = "hi";
+        // std::mbstate_t state = std::mbstate_t();
+        // std::size_t size = std::mbsrtowcs(nullptr, &mbstr, 0, &state);
+        // ASSERT_EQ(2, size);
+
+        ASSERT_EQ(L"", openapi::StringT(""));
+        ASSERT_EQ(L"hi", openapi::StringT("hi"));
+
+        ASSERT_EQ(std::string(""), openapi::ToStdString(L""));
+        ASSERT_EQ(std::string("hi"), openapi::ToStdString(L"hi"));
+#else
+        ASSERT_EQ("", openapi::StringT(""));
+        ASSERT_EQ("hi", openapi::StringT("hi"));
+
+        ASSERT_EQ(std::string(""), openapi::ToStdString(""));
+        ASSERT_EQ(std::string("hi"), openapi::ToStdString("hi"));
+#endif
+}
